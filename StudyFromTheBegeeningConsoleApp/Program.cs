b00d6370348cedc74a8c9e7c924bcb382e2745b0
kirillkhlopkoff <114176969+kirillkhlopkoff@users.noreply.github.com>
2023-07-10 11:43:37 +0300

    прохожу курс для новичков что бы повспоминать и заполнить пробелы в теории

---
+++ StudyFromTheBegeeningConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Имя первого игрока:");
+string Player1= Console.ReadLine();
+Console.WriteLine("Имя второго игрока:");
+string Player2= Console.ReadLine();
+Random random = new Random();
+int firstValue = random.Next(12, 121);
+for (int i = 0; i < firstValue; i++)
+{
+    Console.WriteLine($"Загаданное число: {firstValue}");
+    Console.WriteLine($"{Player1} вводит число:");
+    int enterValue1=int.Parse(Console.ReadLine());
+    firstValue-= enterValue1;
+    Console.WriteLine($"{Player2} вводит число:");
+    int enterValue2 = int.Parse(Console.ReadLine());
+    firstValue-=enterValue2;
+    if(firstValue<=0)
+    {
+        Console.WriteLine("Конец!");
+        break;
+    }
+
+}
+Console.ReadKey();

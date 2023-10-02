37b48061a8f3f55c0d84df1331d1a282171ba62b
kirillkhlopkoff <114176969+kirillkhlopkoff@users.noreply.github.com>
2023-10-02 14:09:35 +0300

    Dictionary

---
+++ Anon_Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> myDic = new Dictionary<string, string>();
+
+        myDic.Add("Машина", "Тесла");
+        myDic.Add("Дом", "Коттедж");
+        myDic.Add("Собака", "Шпитц");
+        myDic.Add("Страна", "Ирландия");
+
+        Console.WriteLine("\nПары");
+        foreach (KeyValuePair<string, string> pair in myDic) { Console.WriteLine($"{pair}"); }
+
+        Console.ReadLine();
+
+
+    }
+}
--- StudyFromTheBegeeningConsoleApp/Program.cs
+++ StudyFromTheBegeeningConsoleApp/Program.cs
@@ -1,45 +1,12 @@
-﻿// многомерный массив - можно представить как лобавление новых измерений, то есть 2-мерный, 3-мерный, 4-мерный и тд
-// главное здесь не забываать, что память для массивов ограничена 2ГБ, а так же максимальным значение типа данных массиваа.
-// указывается через запятые в квадратных скобках [,]
-int[,,] matrix3D = new int[2, 6, 4];
-Console.WriteLine($"matrix3D.Rank = {matrix3D.Rank}");
-Console.WriteLine($"matrix3D.Length = {matrix3D.Length}");
-Console.WriteLine($"matrix3D.GetLength(0) = {matrix3D.GetLength(0)}");
-Console.WriteLine($"matrix3D.GetLength(1) = {matrix3D.GetLength(1)}");
-Console.WriteLine($"matrix3D.GetLength(2) = {matrix3D.GetLength(2)}");
-
-Console.ReadLine();
-
-// зубчатые массивы или массивы массивов - представляют собой массив состоящий из неравномерных массивов, каждый может иметь свою длину.
-// указывается квадратными скобками после квадратных скобок [][].
-int[][] jaggedArray = new int[3][];
-
-jaggedArray[0] = new int[2];
-jaggedArray[0][0] = 1;
-jaggedArray[0][1] = 4;
-
-jaggedArray[1]= new int[3];
-jaggedArray[1][0] = 5;
-jaggedArray[1][1] = 6;
-jaggedArray[1][2] = 1;
-
-jaggedArray[2]= new int[5];
-jaggedArray[2][0] = 2;
-jaggedArray[2][1] = 6;
-jaggedArray[2][2] = 1;
-jaggedArray[2][3] = 4;
-jaggedArray[2][4] = 3;
-
-for (int i = 0; i < jaggedArray.Length; i++)
+﻿class Program
 {
-	for (int j = 0; j < jaggedArray[i].Length; j++)
-	{
-        Console.Write($"{jaggedArray[i][j]} ");
+    public delegate void MyDelegate();
+    static void Main()
+    {
+        MyDelegate mD = new MyDelegate(delegate
+        {
+            Console.WriteLine("Hello, Dear Friend");
+        });
+        mD();
     }
-    Console.WriteLine();
 }
-Console.ReadLine();
-
-
-// дальше был урок про foreach, там его использовали для перебора массива массивов, как элемент из коллекции
-// на практике же я его использовал часто в представлениях, что бы вывести, например, элементы из базы данных на страницу.
